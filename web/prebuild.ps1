e344d776f4ab56de46fb081e2688182a644ecc8e
Vincent (Anapher) <anapher@protonmail.com>
2019-04-04 11:57:02 +0200

    Use local env file for commit info

--- web/prebuild.ps1
+++ web/prebuild.ps1
@@ -1,7 +1,4 @@
 $gitInfo = (git log -1 --pretty=format:"%h%d at %ai") | Out-String;
 
-$env = Get-Content ".\.env";
-$env[1] = "REACT_APP_GIT_INFO=$gitInfo"
-
-$env -join "`n" | Set-Content -Path ".\.env" -NoNewline -Force
+"REACT_APP_GIT_INFO=$gitInfo" | Set-Content -Path ".\.env.local" -NoNewline -Force
 Write-Host "Update Git Info: $gitInfo";
