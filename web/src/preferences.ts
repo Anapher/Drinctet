91b8ccf47733d0e8db7a81e9e1ef3d0f32523d10
Vincent (Anapher) <anapher@protonmail.com>
2019-04-05 17:48:41 +0200

    add csw cards, sort preferences

--- web/src/preferences.ts
+++ web/src/preferences.ts
@@ -1,32 +1,38 @@
 export const socialMediaPlatforms = ["Snapchat", "Instagram", "Facebook"];
 
 export const defaultSources = [
-    "cards/ichundderalkohol.xml",
-    "cards/bullshitfact.xml",
-    "cards/ConversationStartersWorld.NeverHaveIEver.xml",
     "cards/Bevil.xml",
+    "cards/bullshitfact.xml",
+    "cards/common.xml",
+    "cards/ConversationStartersWorld.FunnyQuestions.xml",
+    "cards/ConversationStartersWorld.NeverHaveIEver.xml",
     "cards/ConversationStartersWorld.TruthOrDareDare.xml",
     "cards/ConversationStartersWorld.TruthOrDareTruth.xml",
     "cards/ConversationStartersWorld.Wyr.xml",
-    "cards/common.xml",
+    "cards/ichundderalkohol.xml",
     "cards/picolo/Picolo.xml",
     "cards/picolo/Picolo.Silly.xml",
     "cards/realfacts.xml",
 ];
 
 export const slideWeights: { [key: string]: number } = {
-    FactSlide: 0.3,
     DownSlide: 0.1,
-    NeverEverSlide: 0.5,
-    TruthOrDareSlide: 0.35,
-    WouldYouRatherSlide: 0.5,
     DrinkSlide: 0.6,
-    VirusSlide: 0.2,
-    NoIdeaLosesSlide: 0.3,
+    FactSlide: 0.3,
     GroupGameSlide: 0.3,
+    NeverEverSlide: 0.5,
+    NoIdeaLosesSlide: 0.3,
     TaskSlide: 0.2,
+    TruthOrDareSlide: 0.35,
+    VirusSlide: 0.2,
+    WouldYouRatherSlide: 0.5,
 };
 
 // if a card has one of these tags and an arranged player is selected
 // the propability that their counterpart is selected is increased further
 export const higherArrangementPropabilityTags = ["sexual"];
+
+// everyone uses what he can do best to get females
+export const automaticArrangements = {
+    "Vincent": ["Melina"],
+}
